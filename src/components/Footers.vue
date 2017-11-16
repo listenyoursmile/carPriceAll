8473106f3075462f7c5470a9249b9dd6f676479f
listenyoursmile <1406447997@qq.com>
2017-11-16 11:16:50 +0800

    路由跳转有问题

--- src/components/Footers.vue
+++ src/components/Footers.vue
@@ -1,15 +1,17 @@
 <template>
+	<div><router-link :to="/Detail"></router-link></div>
 	<div>
 		<tabbar style="position: fixed">
 	      <tabbar-item>
 	        <img slot="icon" src="../assets/images/icon_nav_button.png">
 	        <span slot="label">Wechat</span>
 	      </tabbar-item>
-	      <tabbar-item show-dot>
+	      <tabbar-item show-dot link="/SecondCar">
 	        <img slot="icon" src="../assets/images/icon_nav_msg.png">
 	        <span slot="label">Message</span>
+	        
 	      </tabbar-item>
-	      <tabbar-item selected link="/component/demo">
+	      <tabbar-item selected link="/SecondCar">
 	        <img slot="icon" src="../assets/images/icon_nav_article.png">
 	        <span slot="label">Explore</span>
 	      </tabbar-item>
---
+++ src/components/SecondCar.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<p>这是二手车页</p>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'SecondCar'
+	}
+</script>
+<style>
+	p{
+		padding-top: 100px !important;
+	}
+</style>
