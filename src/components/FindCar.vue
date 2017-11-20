56ee262eca3d925ffc2271053680718f836356f4
Administrator <Administrator@USER-20160718VY>
2017-11-20 19:10:36 +0800

    修发现页

--- src/components/FindCar.vue
+++ src/components/FindCar.vue
@@ -43,7 +43,7 @@
 
 <style>
 	/*头部*/
-	.top_photo{height:3.2rem}
+	/*.top_photo{height:3.2rem}
 	.find_huo{width: 100%;background: #f6f6f6;padding:3%;overflow: hidden;font-size: 0.2rem;}
 	.find_huo .huo_tital{width:45%;float: left;background: #fff;height: 2rem;}
 	.find_huo .huo_tital img{width: 30%;float: left;margin-right: 4px;margin:10% 5%;}
@@ -51,21 +51,21 @@
 	.find_huo .huo_tital p b{display: block;margin-top:12%;}
 	.find_huo .huo_tital p i{width:100%;overflow: hidden;text-overflow: ellipsis;white-space:nowrap;height:0.5rem;display: inline-block;}
 	
-	.find_huo .huo_one{margin-right:4%;}
+	.find_huo .huo_one{margin-right:4%;}*/
 	/*选项卡轮播*/
     /*.find_luobo{height:7.5rem;}*/
-    .find_luobo p{height:1rem;line-height: 1rem;font-size: 0.3rem;font-weight: bold;padding: 0 5%;}
+   /* .find_luobo p{height:1rem;line-height: 1rem;font-size: 0.3rem;font-weight: bold;padding: 0 5%;}*/
     /*.find_luobo grid-item img{width: 100%;height:0.5rem;}*/
     /*.grid-center{display: block;text-align: center;color: #666;}*/
-    .find_choose{width: 100%;display: flex;flex-wrap:wrap;}
+    /*.find_choose{width: 100%;display: flex;flex-wrap:wrap;}
     .find_choose li{width:25%;border: 1px solid #ccc;box-sizing: border-box;text-align: center;}
     .find_choose li b{width:70%;margin: 0 auto;height:1rem;display:block;line-height: 1rem;padding-top: 0.2rem;overflow: hidden;}
     .find_choose li img{width:60%;margin: 0 auto;}
     .find_choose li i{font-size:0.1rem;font-weight: 100;}
-    .find_luobo em{width: 100%;background:#f6f6f6;height:0.2rem;display: block;}
+    .find_luobo em{width: 100%;background:#f6f6f6;height:0.2rem;display: block;}*/
      /*买车服务*/
     /*.find_server{height:7.5rem;}*/
-    .find_server p{height:1rem;line-height: 1rem;font-size: 0.3rem;font-weight: bold;padding: 0 5%;}
+    /*.find_server p{height:1rem;line-height: 1rem;font-size: 0.3rem;font-weight: bold;padding: 0 5%;}
     .find_server .server_choose{width: 100%;display: flex;flex-wrap:wrap;}
     .find_server .server_choose li{width:25%;border: 1px solid #ccc;box-sizing: border-box;text-align: center;}
     .find_server .server_choose li b{width:70%;margin: 0 auto;height:1rem;display:block;line-height: 1rem;padding-top: 0.2rem;overflow: hidden;background: cover;}
@@ -76,25 +76,25 @@
     .find_server .server_title img{width:30%;float: left;}
     .find_server .server_title p{width: 60%;display: inline-block;float: left;padding-top: 3%;}
     .find_server .server_title p i{display:block;line-height: 0.6rem;}
-    
+    */
     
     /*汽车用品*/
-   .find_yong p{height:1rem;line-height: 1rem;font-size: 0.3rem;font-weight: bold;padding: 0 5%;}
+   /*.find_yong p{height:1rem;line-height: 1rem;font-size: 0.3rem;font-weight: bold;padding: 0 5%;}
    .find_yong .yong_choose{width: 100%;overflow: hidden;}
    .find_yong .yong_choose li{width:25%;text-align: center;float: left;font-size: 0.1rem;}
    .find_yong .yong_choose li b{width: 100%;}
    .find_yong .yong_choose li img{width: 80%;height:1.5rem;}
    .find_yong .yong_choose li b{display: block;font-weight: 100;color: red;}
-   .find_yong  strong{width: 100%;background:#f6f6f6;height:0.2rem;display: block;}
+   .find_yong  strong{width: 100%;background:#f6f6f6;height:0.2rem;display: block;}*/
    
    /*猜你喜欢*/
-   .find_like{margin-bottom:2rem;}   /*固定底部*/
-   .find_like p{height:1rem;line-height: 1rem;font-size: 0.3rem;font-weight: bold;padding: 0 5%;}
+   /*.find_like{margin-bottom:2rem;}   /*固定底部*/
+   /*.find_like p{height:1rem;line-height: 1rem;font-size: 0.3rem;font-weight: bold;padding: 0 5%;}
    .find_like .like_choose{width: 100%;overflow: hidden;}
    .find_like .like_choose li{float: left;width:50%;box-sizing: border-box;border: 1px solid #ccc;text-align: center;font-size: 0.1rem;}
    .find_like .like_choose li b{width: 100%;}
    .find_like .like_choose li img{width: 80%;margin:0 auto;height: 2rem;}
-   .find_like .like_choose li b{display: block;font-weight: 100;color: red;}
+   .find_like .like_choose li b{display: block;font-weight: 100;color: red;}*/
    
 
   
